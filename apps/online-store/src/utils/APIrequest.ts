7df9af252178c9e2eb493a247b57c094389e27ae
Nina Vasilieva <nina.vasilieva@vitamojo.com>
2022-06-22 12:43:59 +0300

    resolve conflict in Apirequest

--- apps/online-store/src/utils/APIrequest.ts
+++ apps/online-store/src/utils/APIrequest.ts
@@ -1,16 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-export const getData = (endpoint: string) => {
-  axios({
-    method: 'get',
-    url: `http://localhost:3333/api/${endpoint}`,
-    responseType: 'stream'
-  }).then((res) => console.log(res));
-};
-
-export default getData;
-=======
 const API = axios.create({ baseURL: 'http://localhost:3333' });
 
 export const getData = async (endPoint: string) => {
@@ -21,4 +10,3 @@ export const getData = async (endPoint: string) => {
     console.log(error);
   }
 };
->>>>>>> 10253f50416dda74fa884a30d80e1be648ce67db
