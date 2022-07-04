982ac80fa3ed628723fa34a8e5ff81b6eec69089
MihaiLuca95 <lucamihai1995@gmail.com>
2022-07-04 09:41:09 +0300

    clear a little bit code API request

--- apps/online-store/src/utils/APIrequest.ts
+++ apps/online-store/src/utils/APIrequest.ts
@@ -5,10 +5,8 @@ const API = axios.create({ baseURL: 'http://localhost:3333' });
 export const getData = async (endPoint: string) => {
   try {
     const res = await API.get(endPoint);
-    // console.log(res);
     return res;
   } catch (error) {
-    console.log(error);
     return error;
   }
 };
